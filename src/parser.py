e4b3f82afc6672e754297ad5ee508b8657aa01bb
Su Sotoudeh <reza.sotoudeh.van@gmail.com>
2026-02-18 12:01:36 +0330

    implement FASTA reader function

--- src/parser.py
+++ src/parser.py
@@ -1,3 +1,13 @@
 
 # parser.py
 # Functions for reading FASTA and GFF files
+
+from Bio import SeqIO
+
+def read_fasta(fasta_path):
+    """
+    Reads a FASTA file and returns sequence as a string.
+    """
+    record = next(SeqIO.parse(fasta_path, "fasta"))
+    return str(record.seq)
+
